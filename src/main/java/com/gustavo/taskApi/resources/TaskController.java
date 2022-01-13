c7e618d5df425e6639bf7edf0394a701c86dc42a
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-01-12 21:12:07 -0300

    Criando a busca de Task por id

--- src/main/java/com/gustavo/taskApi/resources/TaskController.java
+++ src/main/java/com/gustavo/taskApi/resources/TaskController.java
@@ -6,6 +6,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,13 @@ public class TaskController {
 	@Autowired
 	private TaskService taskService;
 	
+	@GetMapping(value = "{id}")
+	public ResponseEntity<TaskDto> find(@PathVariable Long id) {
+		TaskDto obj = taskService.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	
 	@PostMapping()
 	public ResponseEntity<Void> insert(@RequestBody @Valid TaskNewDto taskDto) {
 		TaskDto obj = taskService.insert(taskDto);
@@ -30,6 +39,6 @@ public class TaskController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-	}	
+	}
 
 }
--- src/main/java/com/gustavo/taskApi/services/TaskService.java
+++ src/main/java/com/gustavo/taskApi/services/TaskService.java
@@ -1,6 +1,7 @@
 package com.gustavo.taskApi.services;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import com.gustavo.taskApi.dtos.TaskNewDto;
 import com.gustavo.taskApi.entities.Task;
 import com.gustavo.taskApi.entities.User;
 import com.gustavo.taskApi.repositories.TaskRepository;
+import com.gustavo.taskApi.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TaskService {
@@ -20,12 +22,23 @@ public class TaskService {
 	@Autowired
 	private UserService userService;
 	
+	public TaskDto find(Long id) {
+		return new TaskDto(findById(id));
+	}
+	
 	public TaskDto insert(TaskNewDto dto) {
 		User user = userService.findById(dto.getUserId());
 		Task task = new Task(null, dto.getTitle(), dto.getDescription(), LocalDate.now(), null, user);
 		
 		task = taskRepository.save(task);
 		return new TaskDto(task);
+	}
+	
+	public Task findById(Long id) {
+		Optional<Task> taskOptional = taskRepository.findById(id);
+		Task task = taskOptional.orElseThrow(() -> new ObjectNotFoundException( "Objeto não encontrado! Id: " + id + ", Tipo: " + Task.class.getName()));
+	
+		return task;
 	}
 
 }
